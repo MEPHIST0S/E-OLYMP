0fc01dec83a86c59cdbdbde5e33350af4892fad0
Murad Babayev <145931157+MEPHIST0S@users.noreply.github.com>
2024-06-07 19:53:44 +0400

    Commited

--- 01616.py
+++ 01616.py
@@ -15,3 +15,28 @@ else:
         print("No")
     else:
         print("Yes")
+
+#Solution via Function
+import math
+
+n = int(input())
+
+def IsPrime(n):
+    count = 0
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                count += 1
+                break  
+
+    if count > 0:
+        return False
+    else:
+        return True
+
+if IsPrime(n):
+    print("Yes")
+else:
+    print("No")
